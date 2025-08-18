e515fa895ec70c7d2a26da6df1a0c5c7b64c661c
DavidDominguez-11 <dominguezdavid396@gmail.com>
2025-08-18 17:44:47 -0600

    Add GET projects/in-progress

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -3,10 +3,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProjects, getFinishedProjects } = require('../controllers/projectController');
+const { getProjects, getFinishedProjects, getInProgressProjects } = require('../controllers/projectController');
 
 router.get('/projects', getProjects);
 router.get('/projects/finished', getFinishedProjects);
+router.get('/projects/in-progress', getInProgressProjects);
 
 module.exports = router;
 
