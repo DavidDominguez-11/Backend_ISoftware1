ba6c27d32c2170964cf48381762d8918101459e2
DavidDominguez-11 <dominguezdavid396@gmail.com>
2025-09-23 13:12:54 -0600

    feat: add POST /clients/create endpoint for creating customers

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -3,9 +3,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getClientes } = require('../controllers/clientesController');
+const { getClientes, createCliente } = require('../controllers/clientesController');
 
 // GET /services/clientes
 router.get('/clients', getClientes);
+
+// POST /services/clients/create
+router.post('/clients/create', createCliente);
 
 module.exports = router;
