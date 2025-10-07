e7a13977317b47a4136d9081bed3744f487adb39
DavidDominguez-11 <dominguezdavid396@gmail.com>
2025-10-07 17:12:40 -0600

    fix(clientes): correct getClientsCount endpoint in src/controllers/clientesController.js  to query COUNT and return proper JSON/404 handling

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsersInfo, getClientsCount } = require('../controllers/userController');
+const { getUsersInfo } = require('../controllers/userController');
 
 router.get('/users_info', getUsersInfo)
-router.get('/clients_count', getClientsCount)
+
 
 module.exports = router;
