0317a4da66142fe302b1ed77a78886f77a049c9c
DavidDominguez-11 <dominguezdavid396@gmail.com>
2025-11-13 13:05:02 -0600

    Deploy changes ESTE SI

--- src/server.js
+++ src/server.js
@@ -1,21 +1,8 @@
 require('dotenv').config();
-const fs = require('fs');
-const https = require('https');
+
 const app = require('./app');
 
-// ⚙️ Configura el puerto
-const HTTPS_PORT = 4000;
-
-// 🧠 Tu dominio
-const DOMAIN = 'poolcenter.fun';
-
-// 🔐 Certificados SSL de Cloudflare
-const sslOptions = {
-  key: fs.readFileSync('/etc/ssl/private/cloudflare-origin.key'),
-  cert: fs.readFileSync('/etc/ssl/certs/cloudflare-origin.pem')
-};
-
-// 🚀 Servidor HTTPS principal (puerto 4000)
-https.createServer(sslOptions, app).listen(HTTPS_PORT, '0.0.0.0', () => {
-  console.log(`✅ Servidor HTTPS corriendo en https://${DOMAIN}:${HTTPS_PORT}`);
+const port = process.env.SERVER_PORT_TEST;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
